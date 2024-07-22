2038522bf5222bfc24c7f75d615c484f70bd24c4
Y-miu-Y <y.miura.miura.y@gmail.com>
2024-07-22 22:10:17 +0900

    async...?

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,17 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+
 
 export const Title = () =>{
-
-    const supabaseClient = createClient(
-        import.meta.env.VITE_SUPABASE_URL,
-        import.meta.env.VITE_SUPABASE_ANON_KEY
-    );
-     
-    console.log(supabaseClient);
-
-    const studyRecordClient = async() => await supabaseClient.from('study-record').select();
-
-    console.log(studyRecordClient);
 
     return (
         <>
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import {Title} from "./components/title";
 import { StudyRecord } from './StudyRecord';
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseClient = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+console.log(supabaseClient);
+
+const studyRecordClient = async() => (
+  { data, error } = await supabaseClient
+      .from('countries')
+      .select()
+);
+
+console.log(studyRecordClient.data);
+
+// ----------------------------------------------------------
 const root = createRoot(document.getElementById("root"));
-
 
 root.render(
   <StrictMode>
